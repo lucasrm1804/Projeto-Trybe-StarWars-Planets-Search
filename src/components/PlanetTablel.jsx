b87ab66c0b6ad502e0f50cfd8919b32ce96fb066
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-01-26 23:22:18 -0300

    Requisito 3 - implementa filtros comparativos

--- src/components/PlanetTablel.jsx
+++ src/components/PlanetTablel.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import GlobalContext from '../context/GlobalContex';
 
 function PlanetTablel() {
-  const { data, search } = useContext(GlobalContext);
+  const { data, search, filter } = useContext(GlobalContext);
   const tableTitles = ['Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate',
     'Gravity', 'Terrain', 'Surface Water', 'Population', 'Films', 'Created', 'Edited',
     'URL'];
@@ -10,15 +10,26 @@ function PlanetTablel() {
     'gravity', 'terrain', 'surface_water', 'population', 'films', 'created', 'edited',
     'url'];
 
-  const filterData = (planet, searchInput) => {
+  const filterData = (planet, searchInput, filterArr) => {
     if (!planet) return [];
-    if (search) {
+    if (searchInput) {
       return planet.filter((name) => name.name.includes(searchInput));
+    }
+    if (filterArr.length > 0) {
+      const arrData = planet;
+      if (filterArr[0].comparison === 'maior que') {
+        return arrData
+          .filter((plan) => plan[filterArr[0].column] > Number(filterArr[0].value));
+      } if (filterArr[0].comparison === 'menor que') {
+        return arrData
+          .filter((plan) => plan[filterArr[0].column] < Number(filterArr[0].value));
+      }
+      return arrData.filter((plan) => plan[filterArr[0].column] === (filterArr[0].value));
     }
     return planet;
   };
 
-  const filterArray = filterData(data, search);
+  const filterArray = filterData(data, search, filter);
 
   return (
     <table>
